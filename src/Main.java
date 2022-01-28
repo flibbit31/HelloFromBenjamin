1d0728e91bfac1b5ed2c7a536541f7090cf95894
benjaminpalmer1 <benjamin.k.palmer@gmail.com>
2022-01-27 21:50:00 -0700

    Responded to your salutations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Benjamin.");
+        System.out.println("Hello Benjamin, my name is Benjamin Palmer.");
     }
 }
