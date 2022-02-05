c0fc4c8dd17f0c68850726840fea37a68dcbb8ce
flibbit31 <flibbit31@yahoo.com>
2022-02-05 13:45:30 -0700

    Set java file back to normal.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Benjamin.");
         System.out.println("Hello Benjamin, my name is Benjamin Palmer.");
-        System.out.println("Hello Benjamin Palmer, I like cheese.");
-
     }
-
 }
